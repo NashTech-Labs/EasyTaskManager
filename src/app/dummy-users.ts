947d952e3d21461f7de4409a1551d261ad71f2c4
arjunpandt <arjun.pandit@nashtechglobal.com>
2024-10-24 10:09:03 +0530

    first commit

---
+++ src/app/dummy-users.ts
@@ -0,0 +1,32 @@
+export const DUMMY_USERS = [
+    {
+      id: 'u1',
+      name: 'Antra',
+      avatar: 'user-1.jpg',
+    },
+    {
+      id: 'u2',
+      name: 'Nikki',
+      avatar: 'user-2.jpg',
+    },
+    {
+      id: 'u3',
+      name: 'Aditya',
+      avatar: 'user-3.jpg',
+    },
+    {
+      id: 'u4',
+      name: 'David Miller',
+      avatar: 'user-4.jpg',
+    },
+    {
+      id: 'u5',
+      name: 'Priya Singh',
+      avatar: 'user-5.jpg',
+    },
+    {
+      id: 'u6',
+      name: 'Arjun',
+      avatar: 'user-6.jpg',
+    },
+  ];
---
+++ src/app/tasks/tasks.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input } from '@angular/core';
+import { TaskComponent } from "./task/task.component";
+import { NewTaskComponent } from "./new-task/new-task.component";
+import { TasksService } from './tasks.service';
+
+@Component({
+  selector: 'app-tasks',
+  standalone: true,
+  imports: [TaskComponent, NewTaskComponent],
+  templateUrl: './tasks.component.html',
+  styleUrl: './tasks.component.css'
+})
+export class TasksComponent {
+  @Input() userId!: string;
+  @Input() userName!: string;
+
+  isAddingTask = false;
+
+  constructor(private tasksService: TasksService) {}
+
+  get userTasks() {
+    return this.tasksService.getUserTasks(this.userId);
+  }
+
+  onStartAddTask() {
+    this.isAddingTask = true;
+  }
+
+  onCloseAddTask() {
+    this.isAddingTask = false;
+  }
+}
